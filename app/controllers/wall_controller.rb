26832e9b07e4374d8c1a322654ab4933b0a37186
qwerqy <amnrsln@gmail.com>
2018-10-06 18:30:13 +0800

    added reloader controller

--- app/controllers/wall_controller.rb
+++ app/controllers/wall_controller.rb
@@ -1,4 +1,10 @@
 class WallController < ApplicationController
   def index
   end
+
+  def reloader
+    respond_to do |format|
+      format.html { render partial: 'wall/show'}
+    end
+  end
 end
